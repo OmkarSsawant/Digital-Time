9473f46bbfa3a623a8b9c78f4b9a2df54dd1f553
OmkarSsawant <omkar18sawant@gmail.com>
2022-06-10 16:27:56 +0530

    Intial Setup

---
+++ app/src/main/java/com/visionDev/digital_time/service/GeoFenceListener.java
@@ -0,0 +1,12 @@
+package com.visionDev.digital_time.service;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class GeoFenceListener extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
